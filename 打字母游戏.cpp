d50cf4fa6525b735564ee476b048deac971e1c4c
Cody <118458506+CuandHNO338324@users.noreply.github.com>
2023-03-07 16:32:23 +0800

    TypingGameInit
    Init Game

---
+++ 打字母游戏.cpp
@@ -0,0 +1,229 @@
+﻿// 打字母游戏.cpp : 定义应用程序的入口点。
+//
+
+#include "framework.h"
+#include "打字母游戏.h"
+
+#define MAX_LOADSTRING 100
+
+COLORREF color = RGB(198, 25, 180);
+int health = 3;
+int score = 0;//分数
+unsigned long times=0;//游戏运行时间
+char c;         // 待输入字符
+WCHAR str[64];//输出的字符串（最多输入64个字符
+char hit;//当前玩家的按键字母
+
+// 全局变量:
+HINSTANCE hInst;                                // 当前实例
+WCHAR szTitle[MAX_LOADSTRING];                  // 标题栏文本
+WCHAR szWindowClass[MAX_LOADSTRING];            // 主窗口类名
+
+// 此代码模块中包含的函数的前向声明:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 在此处放置代码。
+
+    c = 'A' + rand() % 26;//随机生成一个大写字母
+    times=GetTickCount();//程序启动时的时间
+
+    // 初始化全局字符串
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 执行应用程序初始化:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY));
+
+    MSG msg;
+
+    // 主消息循环:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  函数: MyRegisterClass()
+//
+//  目标: 注册窗口类。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   函数: InitInstance(HINSTANCE, int)
+//
+//   目标: 保存实例句柄并创建主窗口
+//
+//   注释:
+//
+//        在此函数中，我们在全局变量中保存实例句柄并
+//        创建和显示主程序窗口。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 将实例句柄存储在全局变量中
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 分析菜单选择:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+
+    case WM_KEYDOWN://对玩家输入的键盘消息进行响应
+        if (health <= 0)//如果玩家生命值小于等于0，游戏结束,退出游戏
+        {
+			break;
+		}
+        hit = wParam;//记录玩家输入的键盘消息
+        if (hit == c)//如果玩家输入的键盘消息与随机生成的大写字母相同
+        {
+			score  ++;//玩家得分加1
+			c  ='A'+rand()%26;//随机生成一个大写字母
+			//times=GetTickCount();//记录程序启动时的时间
+		}
+        else//如果玩家输入的键盘消息与随机生成的大写字母不同
+        {
+			health  --;//玩家生命值减1
+            if(health<=0)//如果玩家生命值小于等于0，游戏结束,退出游戏
+                times=GetTickCount()-times;//记录游戏总耗时
+		}
+        InvalidateRect(hWnd,NULL,TRUE);//重绘屏幕
+        break;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: 在此处添加使用 hdc 的任何绘图代码...
+            SetTextColor(hdc, color);//设定文字的颜色
+            TextOut(hdc,0,0,L"请输入正确的字母！",9);//让玩家输入字母
+            swprintf(str,64,L"生命数：%d",health);//将玩家的生命数转换为字符串
+            TextOut(hdc,200,0,str,wcslen(str));//显示玩家的生命数
+
+            swprintf(str,64,L"得分：%d",score);//将玩家的得分转换为字符串
+            TextOut(hdc,400,0,str,wcslen(str));//显示玩家的得分
+            if (health <= 0)//如果玩家生命值小于等于0，游戏结束
+            {
+                swprintf(str, 64, L"游戏结束，打字速度：%.2f个每秒", float(score * 1000) / times);//将游戏总耗时转换为字符串
+                TextOut(hdc, 600, 0, str, wcslen(str));//显示游戏总耗时
+            }
+            else
+                swprintf(str,64, L"%c", c);
+            TextOut(hdc,60,60,str,wcslen(str));//显示随机生成的大写字母
+
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// “关于”框的消息处理程序。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
